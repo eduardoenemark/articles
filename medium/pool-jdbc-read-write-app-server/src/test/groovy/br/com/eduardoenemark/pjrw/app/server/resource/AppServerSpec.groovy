4b22163c52cf4d2a7c4b4753c94db5b861fad56a
eduardoenemark <eduardoenemark@gmail.com>
2025-08-24 18:02:32 -0400

    revision: setup and cleanup spec.

--- medium/pool-jdbc-read-write-app-server/src/test/groovy/br/com/eduardoenemark/pjrw/app/server/resource/AppServerSpec.groovy
+++ medium/pool-jdbc-read-write-app-server/src/test/groovy/br/com/eduardoenemark/pjrw/app/server/resource/AppServerSpec.groovy
@@ -9,13 +9,17 @@ import org.springframework.http.HttpStatus
 import spock.lang.Shared
 import spock.lang.Specification
 
+import static br.com.eduardoenemark.pjrw.app.server.config.AppConfiguration.LOGGER
+
 //TODO: Making tests...
 class AppServerSpec extends Specification {
 
-
+    @Shared
     final def SERVER_PORT = 8080
+    @Shared
     final def SERVER_URL_BASE = "http://localhost:${SERVER_PORT}"
-
+    @Shared
+    final def PING_URL = "${SERVER_URL_BASE}/ping"
     @Shared
     final def appServerThread = new Thread({
         AppServerInitializer.main(new String[]{"-Dserver.port=${SERVER_PORT}"})
@@ -23,11 +27,31 @@ class AppServerSpec extends Specification {
 
     def setupSpec() {
         appServerThread.start()
-        Thread.sleep(10_000)
+        final def client = new OkHttpClient()
+        final def request = new Request.Builder().url(PING_URL).get().build()
+        while (true) {
+            Call call = client.newCall(request)
+            LOGGER.info("Invoking ping URL: ${PING_URL}")
+            try {
+                try (Response response = call.execute()) {
+                    LOGGER.info("response status: ${response.code()}")
+                    if (call.executed && response.successful) {
+                        LOGGER.info("App Server is ready!")
+                        break
+                    }
+                }
+            } catch (ConnectException ex) {
+                LOGGER.error("App Server is offline: {}", ex.message)
+            }
+            LOGGER.info("Waiting 1s...")
+            Thread.sleep(1_000)
+        }
     }
 
     def cleanupSpec() {
-        appServerThread.interrupt()
+        if (!appServerThread.interrupted) {
+            appServerThread.interrupt()
+        }
     }
 
     def "count product entries"() {
@@ -39,7 +63,7 @@ class AppServerSpec extends Specification {
                 .build()
 
         Call call = client.newCall(request)
-        Response response = call.execute();
+        Response response = call.execute()
 
         expect:
         response.code() == HttpStatus.OK.value()
